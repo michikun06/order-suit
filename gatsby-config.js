9052b43b7bee43e8e11cf283905e55f2ea48ec3b
kawaji michihiro <michikun06@icloud.com>
2020-11-03 14:53:34 +0900

    filesystem修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
+        name: `src`,
         path: path.join(__dirname, `src`, `images`),
       },
     },
