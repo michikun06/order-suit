eb96e52701082595054e4093f6b24a1e314ef461
kawaji michihiro <michikun06@icloud.com>
2020-11-03 17:47:39 +0900

    CMS追加

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-booking-js": () => import("./../../../src/pages/booking.js" /* webpackChunkName: "component---src-pages-booking-js" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-order-js": () => import("./../../../src/pages/order.js" /* webpackChunkName: "component---src-pages-order-js" */),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ipad/react/order-suit/.cache/dev-404-page.js"))),
   "component---src-pages-booking-js": hot(preferDefault(require("/Users/ipad/react/order-suit/src/pages/booking.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/ipad/react/order-suit/src/pages/index.js"))),
   "component---src-pages-order-js": hot(preferDefault(require("/Users/ipad/react/order-suit/src/pages/order.js"))),
